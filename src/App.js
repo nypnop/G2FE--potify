8c1fb3d6358c863ced3c49904db2093e52f13a25
nypnop <13519046@std.stei.itb.ac.id>
2022-03-30 17:03:51 +0700

    homework modul 3 session 1 not done yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Song from './components/track/song.js';
 import './components/track/song.css';
 
+
+const CLIENT_ID = "351d2052bbdb4c60be817eaea55f80ad";
+const ENDPOINT = "https://accounts.spotify.com/authorize";
+const URI_CALLBACK = "http://localhost:3000"
+const SCOPES = "playlist-modify-private";
+
+const getParamsFromAuth = (hash) => {
+  const string = hash.substring(1);
+  const paramsFromURL = string.split("&");
+  const paramsKeyValue = paramsFromURL.reduce((keyurl, valueurl) => {
+    const [key,value] = valueurl.split("=");
+    key[keyurl] = value;
+    return key;
+  }, {});
+
+  return paramsKeyValue;
+}
+
 function App() {
+  useEffect(() => {
+    if(window.location.hash) {
+      const { access_token, expires_in, token_type } = getParamsFromAuth(window.location.hash);
+
+      localStorage.clear();
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
+    }
+  });
+
+  const handleLogin = () => {
+    window.location =  `${ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${URI_CALLBACK}&scope=${SCOPES}&response_type=token&show_dialog=true`
+  }
   return (
-    <Song />
+    <div className='container'>
+      <button onClick={handleLogin}>login</button>
+      <Song />
+
+    </div> 
   );
+    
+  
 }
 
 export default App;
+//reference auth : https://github.com/carmellemillar/carmelle-codes-react-app/blob/main/src/pages/WebApp/WebApp.js
+//still not yet done
+
